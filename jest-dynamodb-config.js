56e45e4a3ddd051870320801386d844fde87ffac
tochiOz <ozurumbatochukwu@yahoo.com>
2024-04-17 10:04:07 -0400

    :star: Jest local test complete

--- jest-dynamodb-config.js
+++ jest-dynamodb-config.js
@@ -1,25 +1,20 @@
 module.exports = {
     tables: [
         {
-            "Table": {
-                "TableName": "player-points-table",
-                "KeySchema": [
-                    {
-                        "AttributeName": "ID",
-                        "KeyType": "HASH"
-                    }
-                ],
-                "AttributeDefinitions": [
-                    {
-                        "AttributeName": "ID",
-                        "AttributeType": "S"
-                    }
-                ],
-                "ProvisionedThroughput": {
-                    "ReadCapacityUnits": 1,
-                    "WriteCapacityUnits": 1
+            TableName: "player-points-table",
+            KeySchema: [
+                {
+                    "AttributeName": "ID",
+                    "KeyType": "HASH"
                 }
-            }
+            ],
+            AttributeDefinitions: [
+                {
+                    "AttributeName": "ID",
+                    "AttributeType": "S"
+                }
+            ],
+            BillingMode: "PAY_PER_REQUEST"
         }
     ]
 }
--- lambdas/common/tests/dynamodb.unit.test.js
+++ lambdas/common/tests/dynamodb.unit.test.js
@@ -1,5 +1,40 @@
-import Dynamo from "../Dynamo";
+const Dynamo = require("../Dynamo");
 
 test('Dynamo is a object', () => {
     expect(typeof Dynamo).toBe('object')
+});
+
+test('dynamo has the get and write function', () => {
+    expect(typeof Dynamo.get).toBe('function');
+    expect(typeof Dynamo.write).toBe('function');
+});
+
+const validTableName = 'player-points-table';
+const data = {
+    ID: '123',
+    score: 22,
+    name: 'David'
+}
+
+test('Dynamo write works', async () => {
+    // this expect there to be at least one assertions by the end of the function
+    // its neccessary because there is a promise here, and we need our test to be able to observe abd wait for the promise to be resolved
+    expect.assertions(1);
+    try {
+        const res = await Dynamo.write(data, validTableName);
+
+        expect(res).toBe(data);
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+test('Dynamo gets works', async () => {
+    expect.assertions(1);
+    try {
+        const res = await Dynamo.get(data.ID, validTableName);
+        expect(res).toEqual(data)
+    } catch (error) {
+        console.log(error)
+    }
 });
