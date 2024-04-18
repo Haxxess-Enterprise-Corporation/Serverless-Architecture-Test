8b328df119b05da0bd72443435851999a6ddaf98
tochiOz <ozurumbatochukwu@yahoo.com>
2024-04-18 09:34:23 -0400

    :star: add spacing

--- lambdas/endpoints/createPlayerScore.js
+++ lambdas/endpoints/createPlayerScore.js
@@ -17,6 +17,7 @@ const pathSchema = yup.object().shape({
 const handler = async event => {
     let ID = event.pathParameters.ID;
     const user = event.body;
+    
     user.ID = ID;
 
     const newUser = await Dynamo.write(user, tableName);
